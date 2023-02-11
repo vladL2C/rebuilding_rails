cbf31ca9c4a4ef2e565e41f739946e626bde067f
Vlad Saraev <vlad.saraev@cultureamp.com>
2023-02-11 17:17:22 +1300

    version 0.0.2

--- rulers/lib/rulers.rb
+++ rulers/lib/rulers.rb
@@ -2,13 +2,24 @@
 
 require_relative "rulers/version"
 require "rulers/array"
+require "rulers/routing"
 
 module Rulers
   class Application
     def call(env)
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
       `echo debug > debug.txt`
       [200, {"Content-Type" => "text/html"},
         ["Hello from Ruby on Rulers!"]]
+    end
+  end
+
+  class Controller
+    attr_reader :env
+
+    def initialize(env)
+      @env = env
     end
   end
 end
