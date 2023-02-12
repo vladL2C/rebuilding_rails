6fb70c4ad0be37e99e2d6297a35210064f23c809
Vlad Saraev <vlad.saraev@cultureamp.com>
2023-02-12 14:12:40 +1300

    update

--- best_quotes/app/controllers/quotes_controller.rb
+++ best_quotes/app/controllers/quotes_controller.rb
@@ -1,5 +1,9 @@
 class QuotesController < Rulers::Controller
   def a_quote
-    "There is nothing either good or bad but thinking makes it so."
+    "There is nothing either good or bad but thinking makes it so." + "\n<pre>\n#{env}\n</pre>"
+  end
+
+  def exception
+    raise "It's a bad one!"
   end
 end
