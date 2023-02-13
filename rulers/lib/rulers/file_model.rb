697285d011afda9d092b0978ac2168544a7be938
Vlad Saraev <vlad.saraev@cultureamp.com>
2023-02-14 11:36:33 +1300

    template

--- rulers/lib/rulers/file_model.rb
+++ rulers/lib/rulers/file_model.rb
@@ -31,6 +31,29 @@ module Rulers
         files = Dir["db/quotes/*.json"]
         files.map { |f| FileModel.new(f) }
       end
+
+      def self.create(attrs)
+        hash = {}
+        hash["submitter"] = attrs["submitter"] || ""
+        hash["quote"] = attrs["quote"] || ""
+        hash["attribution"] = attrs["attribution"] || ""
+        files = Dir["db/quotes/*.json"]
+        names = files.map { |f| f.split("/")[-1] }
+        highest = names.map { |b| b[0...-5].to_i }.max
+        id = highest + 1
+
+        File.write("db/quotes/#{id}.json",
+          <<~TEMPLATE
+            {
+              "submitter": "#{hash["submitter"]}",
+              "quote": "#{hash["quote"]}",
+              "attribution": "#{hash["attribution"]}"
+            }
+          TEMPLATE
+        )
+
+        FileModel.new("db/quotes/#{id}.json")
+      end
     end
   end
 end
